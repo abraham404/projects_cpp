19953b9baa1775f4ff0bfb1fe5fe993807ef6894
Jorge Rosas Windows <abraham.work7@gmail.com>
2022-12-03 19:40:51 -0700

    Ejemplo de plantillas en c++

--- project1.cpp
+++ project1.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+//Ejemplo de plantillas en c++ para 
+
+/*
+Una plantilla de función es una función única completa que sirve como modelo para
+una familia de funciones. C
+*/
+
+template <class T>
+void mostrarabs(T numero){
+
+    if (numero<0){
+        numero = -numero;
+        cout<<"El valor absoluto es: "
+            <<numero<<endl;
+    }
+}
+
 int main(){
 
-    cout<<"Hello Abraham"<<endl;
-    
+    int num1 = -4;
+    float num2 = -4.25f;
+    double num3 =  -4.23456;
+
+    mostrarabs(num1);
+    mostrarabs(num2);
+    mostrarabs(num3);
+
     return 0;
 }
